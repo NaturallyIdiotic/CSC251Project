db70a2cad9725b259b7f37ae3c8274fcbe4d5590
NaturallyIdiotic <trevorbercher@gmail.com>
2024-09-13 12:15:34 -0400

    Project part 1 Completion
    Completion policy class as well as demo for project part 1

--- Policy.java
+++ Policy.java
@@ -19,7 +19,7 @@ public class Policy
       @param holderAge holds policy holder age
       @param provideName holds name of provider
       @param holderFirstName holds policy holders first name
-      @param holderLastName holds polciy holders last name
+      @param holderLastName holds policy holders last name
       @param smokeStat holds policy holders smoking status
       @param holderHeight holds policy holders height
       @param holderWeight holds policy holders weight
@@ -67,4 +67,184 @@ public class Policy
       
       weight = 0.0;   
    }
+   /**
+      Mutator (setter) method
+      @param pN symbol for policy number
+   
+   */
+   public void setPolicyNumber(int pN)
+   {
+      policyNumber = pN;
+   }
+   /**
+      Accessor (getter) method
+      @return the policy number
+   */
+   public int getPolicyNumber()
+   {
+      return policyNumber;
+   }
+   /** 
+      Mutator (setter) method
+      @param a representing policy holder age
+   */
+   public void setAge(int a)
+   {
+      age = a;
+   }
+   /**
+      Accessor (getter) method
+      @return age of policy holder
+   */
+   public int getAge()
+   {
+      return age;
+   }
+   /** 
+      Mutator (setter) method
+      @param pN representing policy provider name
+   */
+   public void setProviderName(String pN)
+   {
+      providerName = pN;
+   }
+   /**
+      Accessor (getter) method
+      @return name of policy provider
+   */
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   /** 
+      Mutator (setter) method
+      @param fN representing first name of policy holder
+   */
+   public void setFirstName(String fN)
+   {
+      firstName = fN;
+   }
+   /**
+      Accessor (getter) method
+      @return first name of policy holder
+   */
+   public String getFirstName()
+   {
+      return firstName;
+   }
+   /** 
+      Mutator (setter) method
+      @param lN representing last name of policy holder 
+   */
+   public void setLastName(String lN)
+   {
+      lastName = lN;
+   }
+   /**
+      Accessor (getter) method
+      @return last name of policy holder
+   */
+   public String getLastName()
+   {
+      return lastName;
+   }
+   /** 
+      Mutator (setter) method
+      @param s representing smoking status of policy holder
+   */
+   public void setSmokerStatus(String s)
+   {
+      smokerStatus = s;
+   }
+   /**
+      Accessor (getter) method
+      @return smoking status of policy holder
+   */
+   public String getSmokerStatus()
+   {
+      return smokerStatus;
+   }
+   /** 
+      Mutator (setter) method
+      @param h representing policy holder height
+   */
+   public void setHeight(double h)
+   {
+      height = h;
+   }
+   /**
+      Accessor (getter) method
+      @return height of policy holder
+   */
+   public double getHeight()
+   {
+      return height;
+   }
+   /** 
+      Mutator (setter) method
+      @param w representing policy holder weight
+   */
+   public void setWeight(double w)
+   {
+      weight = w;
+   }
+   /**
+      Accessor (getter) method
+      @return weight of policy holder
+   */
+   public double getWeight()
+   {
+      return weight;
+   }
+   /**
+      Method that returns the BMI of the policy holder
+      @return BMI the body mass index of policy holder
+   */
+   public double getBMI()
+   {
+      double bmi = 0.0;
+      bmi = ( weight * 703 ) / ( height * height);
+      return bmi;
+   }
+   /**
+      Method that calculates and returns the price of the insurance policy
+      @return policyPrice the total price of the policy 
+   */
+   public double getInsurancePolicyPrice()
+   {
+      
+      final double INSURANCE_BASE_FEE = 600.0;
+      final double AGE_FEE = 75.0;
+      final double SMOKER_FEE = 100.0;
+      
+      double bmi = ( weight * 703 ) / ( height * height);
+      double bmiFee = ( bmi - 35) * 20; 
+      
+      double policyPrice = 0.0;
+      
+      // adds base cost of insurance to total policy price
+      policyPrice += INSURANCE_BASE_FEE;
+      
+      // checks if holders age is over 50 and adds age fee if applicable
+      if(age > 50)
+      {
+         policyPrice += AGE_FEE;
+      }
+      
+      // checks smoker status and if the holder is a smoker the smoking fee is added
+      if(smokerStatus.equalsIgnoreCase("Smoker"))
+      {
+         policyPrice += SMOKER_FEE;
+      }
+      
+      if(bmi > 35)
+      {
+         policyPrice += bmiFee;
+      }
+      
+      return policyPrice;
+   }
+   
+                     
+   
 }
--- Project_Trevor_Bercher.java
+++ Project_Trevor_Bercher.java
@@ -1,4 +1,83 @@
+import java.util.Scanner;
+
 public class Project_Trevor_Bercher
 {
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);
+      
+      int policyNumber = 0, age = 0;
+      
+      String providerName, firstName, lastName, smokerStatus;
+      
+      double height = 0.0, weight = 0.0, totalPolicyPrice = 0.0, bmi = 0.0;
+      
+      // User is prompted to enter the following information
+      
+      System.out.println("Please enter the Policy number:");
+      policyNumber = keyboard.nextInt();
+      
+      // Clear buffer
+      keyboard.nextLine();
+      
+      System.out.println("Please enter the Provider Name: ");
+      providerName = keyboard.nextLine();
+      
+      System.out.println("Please enter the Policyholder's First Name: ");
+      firstName = keyboard.nextLine();      
+      
+      System.out.println("Please enter the Policyholder's Last Name: ");
+      lastName = keyboard.nextLine();
+     
+      System.out.println("Please enter the age of the Policyholder: ");
+      age = keyboard.nextInt();
+      
+      while(age < 1)
+      {
+         System.out.println("Error invalid data input, age inputted must be greater than 0.");
+         
+         System.out.println("Please enter the age of the Policyholder: ");
+         age = keyboard.nextInt();         
+      }
+      
+      // Clear buffer
+      keyboard.nextLine();
+                       
+      System.out.println("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
+      smokerStatus = keyboard.nextLine();    
+      
+      while(!smokerStatus.equalsIgnoreCase("smoker") && !smokerStatus.equalsIgnoreCase("non-smoker"))
+      {
+         System.out.println("Invalid data input please enter either smoker or non-smoker");
+         
+         System.out.println("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
+         smokerStatus = keyboard.nextLine();          
+      }  
+      
+      System.out.println("Please enter the PolicyHolder's Height (in inches): ");
+      height = keyboard.nextDouble();
+      
+      System.out.println("Please enter the PolicyHolder's Weight (in pounds): ");    
+      weight = keyboard.nextDouble();   
+      
+      // instantiate policy object
+      
+      Policy policyOne = new Policy(policyNumber, age, providerName, firstName, lastName, smokerStatus, height, weight);
+      
+      // outputting all data fields as well as the BMI and Policy Price
+      
+      System.out.println("Policy Number: " + policyOne.getPolicyNumber());
+      System.out.println("\nProvider Name: " + policyOne.getProviderName());
+      System.out.println("\nPolicyholder's First Name: " + policyOne.getFirstName());
+      System.out.println("\nPolicyholder's Last Name: " + policyOne.getLastName());
+      System.out.println("\nPolicyHolders's Age: "  + policyOne.getAge());
+      System.out.println("\nPolicyHolder's Smoking Status: " + policyOne.getSmokerStatus());
+      System.out.printf("\nPolicyHolder's Height: %.1f\n", policyOne.getHeight());
+      System.out.printf("\nPolicyHolder's Weight: %.1f\n", policyOne.getWeight());
+      System.out.printf("\nPolicyHolder's BMI: %.2f\n", policyOne.getBMI());
+      System.out.printf("\nPolicy Price: $%.2f\n", policyOne.getInsurancePolicyPrice());
+      
+      
 
+   }
 }
